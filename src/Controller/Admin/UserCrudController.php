811a5d796028480e2cce6292b90d9bfa5f1a3465
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-19 15:12:35 +0100

    rollback prep

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -4,6 +4,10 @@ namespace App\Controller\Admin;
 
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class UserCrudController extends AbstractCrudController
 {
@@ -12,14 +16,25 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->setDisabled(),
+            TextField::new('email'),
+            TextField::new('password')->hideWhenUpdating(),
+            ChoiceField::new('roles')->allowMultipleChoices()->setChoices([
+                'Admin' => 'ROLE_ADMIN',
+                'User' => 'ROLE_USER',
+            ]),
+            ImageField::new('picture')
+                ->setUploadDir('public/uploads/user')
+                ->setBasePath('uploads/user')
+                ->setUploadedFileNamePattern('[randomhash].[extension]')
+                ->setFormTypeOptions([
+                    'attr' => [
+                        'accept' => 'image/jpeg, image/png'
+                    ]
+                ])
         ];
     }
-    */
 }
