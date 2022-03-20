5ffdab40beb0c5e52a26d3340a00a204f14bf239
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-20 15:15:53 +0100

    :lipstick: Started Templating

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductCategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class AppController extends AbstractController
 {
     #[Route('/', name: 'app_app')]
-    public function index(): Response
+    public function index(ProductCategoryRepository $productCategoryRepository ): Response
     {
+        $categories = $productCategoryRepository->findAll();
+
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'categories' => $categories
         ]);
     }
 }
