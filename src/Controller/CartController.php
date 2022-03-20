9461d1cdab01c663fdb7f50dd15c12f9043eccc7
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-20 22:50:17 +0100

    :rocket: Cart Done, Interaction with stock WIP

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,8 +2,8 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -12,27 +12,88 @@ use Symfony\Component\Routing\Annotation\Route;
 class CartController extends AbstractController
 {
     #[Route('/cart', name: 'cart')]
-    public function index(): Response
+    public function index(SessionInterface $session, ProductRepository $productRepository): Response
     {
+        $cart = $session->get('cart',[]);
+        $cartData = [];
+        $priceTotal = 0;
+
+        foreach ($cart as $id => $itemQuantity)
+        {
+            $product = $productRepository->find($id);
+            $cartData[] = [
+                'product' => $product,
+                'quantity' => $itemQuantity
+            ];
+            $priceTotal += $product->getPrice() * $itemQuantity;
+        }
+
         return $this->render('cart/index.html.twig', [
             'controller_name' => 'CartController',
+            'cartData' => $cartData,
+            'total' => $priceTotal,
         ]);
     }
+
+    /**
+     * @throws NonUniqueResultException
+     */
     #[Route('/cart/add/{id}', name: 'cart_add')]
     public function add(ProductRepository $productRepository, SessionInterface $session, $id)
     {
         $cart = $session->get('cart', []);
-        $product = $productRepository->findOneById($id);
-        $product_id = $product->getId();
+        $product = $productRepository->findOneById($id)->getId();
 
-        if(!$cart[$product_id])
+        if (!empty($cart[$product]))
         {
-            $cart[$product_id] ++;
+            $cart[$product]++;
         } else {
-            $cart[$product_id] = 1;
+            $cart[$product] = 1;
         }
-
         $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/remove/{id}', name: 'cart_remove')]
+    public function remove(ProductRepository $productRepository, SessionInterface $session, $id)
+    {
+        $cart = $session->get('cart', []);
+        $product = $productRepository->findOneById($id)->getId();
+
+        if (!empty($cart[$product]))
+        {
+           if($cart[$product] > 1) {
+               $cart[$product]--;
+           } else {
+               unset($cart[$product]);
+           }
+        }
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart');
+    }
+
+    #[Route('/cart/delete/{id}', name: 'cart_delete')]
+    public function delete(ProductRepository $productRepository, SessionInterface $session, $id)
+    {
+        $cart = $session->get('cart', []);
+        $product = $productRepository->findOneById($id)->getId();
+
+        if (!empty($cart[$product]))
+        {
+            unset($cart[$product]);
+        }
+        $session->set('cart', $cart);
+
+        return $this->redirectToRoute('cart');
+    }
+    #[Route('/cart/delete/all', name: 'cart_delete_all')]
+    public function deleteAllItem(SessionInterface $session)
+    {
+        $cart = $session->get('cart', []);
+        unset($cart);
+        $session->set('cart', []);
 
         return $this->redirectToRoute('cart');
     }
